dd29a6fdf163996ae87ba3d6a1a11de3e1097ce8
jinho <jinhot@gmail.com>
2018-07-08 23:57:39 +0900

    init

--- start_server.py
+++ start_server.py
@@ -1,6 +1,7 @@
 from gevent.pywsgi import WSGIServer
 from app import create_app
 
+print("WIKIPI version 0.1 alpha.")
 print("gevent server started.")
 http_server = WSGIServer(('0.0.0.0', 3600), create_app())
 http_server.serve_forever()
