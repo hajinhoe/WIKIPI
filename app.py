ac8bfe53fb10b9b3c9fbe0215a22ce040879aa5f
jinho <jinhot@gmail.com>
2018-07-10 21:09:18 +0900

    오류 수정

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def create_app(test_config=None):
         for item in query_items:
             results = \
                 db.execute(
-                    "SELECT name, markdown_data FROM (doc_list join doc using (id)) as t WHERE t.markdown_data like ?",
+                    "SELECT name, markdown_data FROM doc_list join doc using (id) WHERE markdown_data like ?",
                     ("%" + item + "%",)).fetchall()
             if results is not None:
                 for result in results:
@@ -314,7 +314,7 @@ def create_app(test_config=None):
                                          (doc_id,)).fetchone()['max(version)'] + 1
             history_list = (doc_id, history_version, markdown_text)
             db.execute("INSERT into history VALUES(?, ?, ?, datetime('now', 'localtime'))", history_list)
-            db.execute("UPDATE doc SET html_data = ? WHERE id = ?", (html_text, doc_id))
+            db.execute("UPDATE doc SET html_data = ? and markdown_data = ? WHERE id = ?", (html_text, markdown_text, doc_id))
             db.commit()
             return redirect("/doc/{0}".format(doc_name), code=302)
 
